e64c6f08e011e7b129684c32938606abd4a6cf50
Kowith Singkornkeeree <kowith337@hotmail.com>
2019-08-02 01:43:09 +0700

    Generated at 02/08/2019 1:37:22 AM / Patched at 02/08/2019 1:42:52 AM
    Update batch file for allowing terminate of existing process, in case if the previous progress still stuck.

--- generate.bat
+++ generate.bat
@@ -3,5 +3,6 @@ del hosts.txt.bak
 del hosts.txt.tmp
 rd .\bin\hostsmgr\32\cache /s /q
 rd .\bin\hostsmgr\64\cache /s /q
+taskkill /f /t /im hostsmgr.exe
 .\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt
 exit
