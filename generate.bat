67ba216f54d190815d71d37d15e38309a362898f
Kowith Singkornkeeree <kowith337@hotmail.com>
2019-08-03 23:02:30 +0700

    Generated at 03/08/2019 10:46:20 PM / Patched at 03/08/2019 11:00:31 PM
    Change in other files
    - Add ability to log command messages
    - Log file of command message are ignored due to contain my system path

--- generate.bat
+++ generate.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 del hosts.txt.bak
 del hosts.txt.tmp
+del generatelog.txt
 rd .\bin\hostsmgr\32\cache /s /q
 rd .\bin\hostsmgr\64\cache /s /q
 taskkill /f /t /im hostsmgr.exe
-.\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt
+.\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt >generatelog.txt
 exit
