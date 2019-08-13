8094065c2bb3d23194be40f4a198a49a02d31718
Kowith Singkornkeeree <kowith337@hotmail.com>
2019-08-13 18:37:07 +0700

    Generated at 13/08/2019 6:02:08 PM / Patched at 13/08/2019 6:35:56 PM
    Now I've added more commands to regenerate hosts file for 2 passes

--- generate.bat
+++ generate.bat
@@ -5,5 +5,9 @@ del generatelog.txt
 rd .\bin\hostsmgr\32\cache /s /q
 rd .\bin\hostsmgr\64\cache /s /q
 taskkill /f /t /im hostsmgr.exe
-.\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt >generatelog.txt
+echo Start first pass generator
+.\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt >generatelog1.txt
+timeout 10
+echo Start second pass generator (probably will be regenerated with existing cache)
+.\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt >generatelog2.txt
 exit
