df2b8b9bd9eda71a114e784026f729a570901836
Kowith Singkornkeeree <kowith337@hotmail.com>
2019-08-25 18:20:12 +0700

    Generated at 25/08/2019 6:02:46 PM / Patched at 25/08/2019 6:18:27 PM

--- generate.bat
+++ generate.bat
@@ -5,6 +5,8 @@ del generatelog.txt
 rd .\bin\hostsmgr\32\cache /s /q
 rd .\bin\hostsmgr\64\cache /s /q
 taskkill /f /t /im hostsmgr.exe
+echo Wait until I can connect to internet properly...
+timeout 30
 echo Start first pass generator
 .\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt >generatelog1.txt
 timeout 10
