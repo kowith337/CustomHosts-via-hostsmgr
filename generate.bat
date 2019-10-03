f443df4de8e82d01e41c13438884dc69709d4eb0
Kowith Singkornkeeree <kowith337@hotmail.com>
2019-10-03 20:22:36 +0700

    Generated at 03/10/2019 7:35:41 PM / Patched at 03/10/2019 8:21:43 PM
    Also update batch file.

--- generate.bat
+++ generate.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
+echo Terminating previous instance...
 del hosts.txt.bak
 del hosts.txt.tmp
-del generatelog.txt
+del generatelog1.txt
+del generatelog2.txt
 rd .\bin\hostsmgr\32\cache /s /q
 rd .\bin\hostsmgr\64\cache /s /q
 taskkill /f /t /im hostsmgr.exe
