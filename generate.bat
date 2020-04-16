e75221183e063b5b7f971a677db0cbd57e1183dd
Kowith Singkornkeeree <kowith337@hotmail.com>
2020-04-16 12:36:08 +0700

    Generated at 16/04/2020 12:24:00 PM
    Note: in the next generate attempt will begin to use "LF EOL" format because this hosts file may mostly focus to Android and Linux-based OS and devices.

--- generate.bat
+++ generate.bat
@@ -8,8 +8,8 @@ del generatelog2.txt
 rd .\bin\hostsmgr\32\cache /s /q
 rd .\bin\hostsmgr\64\cache /s /q
 echo Start first pass generator
-.\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt >generatelog1.txt
+.\bin\hostsmgr\64\hostsmgr.exe /os linux /path .\hosts.txt >generatelog1.txt
 timeout 10
-echo Start second pass generator (probably will be regenerated with existing cache)
-.\bin\hostsmgr\64\hostsmgr.exe /path .\hosts.txt >generatelog2.txt
+echo Start second pass generator (probably will be regenerate with existing cache)
+.\bin\hostsmgr\64\hostsmgr.exe /os linux /path .\hosts.txt >generatelog2.txt
 exit
