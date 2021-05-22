59b3779f9f824c91ed271fce2c61494748b053ed
Kowith Singkornkeeree <kowith337@hotmail.com>
2021-05-22 16:06:16 +0700

    Generated at 22/05/2021 4:05:32 PM +
    Remove Daniel's AMP hosts since this host file doesn't exist.

--- generate.bat
+++ generate.bat
@@ -10,8 +10,8 @@ rd .\bin\hostsmgr\64\cache /s /q
 copy /y *.dat .\bin\hostsmgr\32
 copy /y *.dat .\bin\hostsmgr\64
 echo Start first pass generator
-.\bin\hostsmgr\64\hostsmgr.exe /os linux /path .\hosts.txt >generatelog1.txt
+.\bin\hostsmgr\64\hostsmgr.exe /os linux /path .\hosts.txt
 timeout 10
 echo Start second pass generator (probably will be regenerate with existing cache)
-.\bin\hostsmgr\64\hostsmgr.exe /os linux /path .\hosts.txt >generatelog2.txt
+.\bin\hostsmgr\64\hostsmgr.exe /os linux /path .\hosts.txt
 exit
